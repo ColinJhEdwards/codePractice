ad411bd399d5f5df21a67f1a7b64729bc4314854
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-12 13:02:27 -0800

    created functions for saving items to local storage and loading said items

--- app.js
+++ app.js
@@ -5,27 +5,33 @@ const todoList = document.querySelector(".todo-list");
 // event listeners
 submitBtn.addEventListener("click", createTodo);
 todoList.addEventListener("click", deleteCheck);
+document.addEventListener("DOMContentLoaded", loadStorage);
 
 //functions
 function createTodo(e) {
-  e.preventDefault();
-  //create div to hold text and btns
-  const todoItem = document.createElement("div");
-  todoItem.classList.add("todo-item");
-  //Create li
-  const item = document.createElement("li");
-  item.innerText = input.value;
-  todoItem.append(item);
-  //    create buttons
-  const completeBtn = document.createElement("button");
-  completeBtn.classList.add("completeBtn");
-  completeBtn.innerText = "O";
-  const trashBtn = document.createElement("button");
-  trashBtn.classList.add("trashBtn");
-  trashBtn.innerText = "X";
-  todoItem.append(completeBtn, trashBtn);
-  todoList.append(todoItem);
-  input.value = "";
+  if (input.value === "") {
+    return;
+  } else {
+    e.preventDefault();
+    //create div to hold text and btns
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item");
+    //Create li
+    const item = document.createElement("li");
+    item.innerText = input.value;
+    todoItem.append(item);
+    saveToStorage(input.value);
+    //    create buttons
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("completeBtn");
+    completeBtn.innerText = "O";
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("trashBtn");
+    trashBtn.innerText = "X";
+    todoItem.append(completeBtn, trashBtn);
+    todoList.append(todoItem);
+    input.value = "";
+  }
 }
 
 function deleteCheck(e) {
@@ -38,4 +44,42 @@ function deleteCheck(e) {
       item.remove();
     });
   }
+}
+
+function saveToStorage(todo) {
+  //Check local storage, is there stuff stored already?
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.push(todo);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadStorage() {
+  let todos;
+  if (localStorage.getItem("todos") === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach((t) => {
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item");
+    //Create li
+    const item = document.createElement("li");
+    item.innerText = t;
+    todoItem.append(item);
+    //    create buttons
+    const completeBtn = document.createElement("button");
+    completeBtn.classList.add("completeBtn");
+    completeBtn.innerText = "O";
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("trashBtn");
+    trashBtn.innerText = "X";
+    todoItem.append(completeBtn, trashBtn);
+    todoList.append(todoItem);
+  });
 }
