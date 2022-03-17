47c529fbf8c56a6de0abe1285a21c7c09d7bc910
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-16 20:31:16 -0700

    added function for checking text contrast using chroma

--- app.js
+++ app.js
@@ -20,7 +20,18 @@ function randomColors() {
     const randomColor = generateHex();
     div.style.backgroundColor = randomColor;
     hexText.innerText = randomColor;
+    // check for contrast
+    checkTextContrast(randomColor, hexText);
   });
+}
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
 }
 
 randomColors();
