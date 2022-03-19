53b9bf9f1cd19e364c21b19211d84fcbc7cc8bcc
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-18 20:39:38 -0700

    created function for updating sliders when colors refresh

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ function randomColors() {
 
     colorizeSliders(color, hue, brightness, saturation);
   });
+  // reset inputs
+  resetInputs();
 }
 
 function checkTextContrast(color, text) {
@@ -99,8 +101,29 @@ function updateTextUI(index) {
   textHex.innerText = color.hex();
   //check text controst
   checkTextContrast(color, textHex);
-  // icons.forEach((icon) => {
-  //   checkTextContrast(icon, color);
-  // });
+  icons.forEach((icon) => {
+    checkTextContrast(color, icon);
+  });
+}
+
+function resetInputs() {
+  const sliders = document.querySelectorAll(".sliders input");
+  sliders.forEach((slider) => {
+    if (slider.name === "hue") {
+      const hueColor = initialColors[slider.getAttribute("data-hue")];
+      const hueValue = chroma(hueColor).hsl()[0];
+      slider.value = Math.floor(hueValue);
+    }
+    if (slider.name === "brightness") {
+      const brightColor = initialColors[slider.getAttribute("data-bright")];
+      const brightValue = chroma(brightColor).hsl()[2];
+      slider.value = Math.floor(brightValue * 100) / 100;
+    }
+    if (slider.name === "saturation") {
+      const satColor = initialColors[slider.getAttribute("data-sat")];
+      const satValue = chroma(satColor).hsl()[1];
+      slider.value = Math.floor(satValue * 100) / 100;
+    }
+  });
 }
 randomColors();
