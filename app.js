a739cbc1812508e21161e981587619473d0c7e10
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-21 19:40:16 -0700

    created basic html and css

---
+++ app.js
@@ -0,0 +1,5 @@
+const api = "563492ad6f91700001000001c52d87070c5547f29ee36618e00d5e4f";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
