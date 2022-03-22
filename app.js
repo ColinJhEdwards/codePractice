1442004825f0385fff175f731196018767610ae2
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-21 20:41:31 -0700

    created function for searching for specific photos based off users input

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const searchInput = document.querySelector(".search-input");
 const submitButton = document.querySelector(".submit-btn");
 let searchValue;
 
+// event listeners
+
+submitButton.addEventListener("click", (e) => {
+  searchPhotos(e);
+});
+
+// functions
+
 async function curatedPhotos() {
   const dataFetch = await fetch(
     `https://api.pexels.com/v1/curated?per_page=15^page=1`,
@@ -16,7 +24,33 @@ async function curatedPhotos() {
     }
   );
   const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(e) {
+  e.preventDefault();
+  searchValue = searchInput.value;
+  console.log(searchValue);
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${searchValue}&per_page=15`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: api,
+      },
+    }
+  );
+  const data = await dataFetch.json();
   console.log(data.photos);
+  gallery.innerHTML = "";
+  searchInput.value = "";
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement("div");
     galleryImg.classList.add("gallery-img");
