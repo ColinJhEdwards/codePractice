0700492cd48326438d7b94625671c2bc8f39022e
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-24 19:19:01 -0700

    created song and player components with basic template for content and installed font awesome react

--- music/src/App.js
+++ music/src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Song from "./components/Song";
+import Player from "./components/Player";
 
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
