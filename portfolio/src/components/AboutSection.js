b4ed0d1359b390fe6571890f9b4fa09ab5c474b8
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-28 19:39:19 -0700

    implemented styled components, set up global styles, and styled aboutsection

--- portfolio/src/components/AboutSection.js
+++ portfolio/src/components/AboutSection.js
@@ -1,9 +1,10 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
+    <StyledDiv className="about">
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -27,8 +28,36 @@ const AboutSection = () => {
       <div className="image">
         <img src={home1} alt="Guy with a camera :)" />
       </div>
-    </div>
+    </StyledDiv>
   );
 };
+
+const StyledDiv = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+  .description {
+    flex: 1;
+    padding-right: 5rem;
+    h2 {
+      font-weight: lighter;
+    }
+  }
+  .hide {
+    overflow: hidden;
+  }
+  .image {
+    flex: 1;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 80vh;
+      object-fit: cover;
+    }
+  }
+`;
 
 export default AboutSection;
