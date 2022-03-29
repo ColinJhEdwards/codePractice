e72f1b9f8a1682fb28699e80d0d6aec7dbb046a7
Colin Edwards <ColinJhEdwards@gmail.com>
2022-03-28 19:59:31 -0700

    styled services section and added fonts from google fonts

--- portfolio/src/components/ServicesSection.js
+++ portfolio/src/components/ServicesSection.js
@@ -4,10 +4,12 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+import styled from "styled-components";
+import { StyledDiv } from "../styles";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
+    <StyledServices className="services">
       <div className="description">
         <h2>
           High <span>quality</span> services
@@ -42,12 +44,58 @@ const ServicesSection = () => {
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
         </div>
-        <div className="image">
-          <img src={home2} alt="home2" />
-        </div>
       </div>
-    </div>
+      <div className="image">
+        <img src={home2} alt="home2" />
+      </div>
+    </StyledServices>
   );
 };
+
+const StyledServices = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+  .description {
+    flex: 1;
+    padding-right: 5rem;
+    .cards {
+      display: flex;
+      flex-wrap: wrap;
+      .card {
+        flex-basis: 20rem;
+        .icon {
+          display: flex;
+          align-items: center;
+          h3 {
+            margin-left: 1rem;
+            background: white;
+            color: black;
+            padding: 1rem;
+          }
+        }
+      }
+    }
+  }
+  .image {
+    flex: 1;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 80vh;
+      object-fit: cover;
+    }
+  }
+`;
 
 export default ServicesSection;
