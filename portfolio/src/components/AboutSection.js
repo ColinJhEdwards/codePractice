ad46c712666b1b0a75e4205371fd8b9117e9a39e
Colin Edwards <ColinJhEdwards@gmail.com>
2022-04-01 19:20:05 -0700

    creating animations for framer motions

--- portfolio/src/components/AboutSection.js
+++ portfolio/src/components/AboutSection.js
@@ -1,24 +1,45 @@
 import React from "react";
 import home1 from "../img/home1.png";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const titleAnimation = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2 } },
+  };
+  const container = {
+    hidden: { x: 100 },
+    visible: {
+      x: 0,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        staggerChildren: 0.5,
+      },
+    },
+  };
   return (
     <StyledDiv className="about">
       <div className="description">
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className="title"
+        >
           <div className="hide">
-            <h2>We work to make </h2>
+            <motion.h2 variants={titleAnimation}>We work to make </motion.h2>
           </div>
           <div className="hide">
-            <h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span>
-            </h2>
+            </motion.h2>
           </div>
           <div className="hide">
-            <h2>come true</h2>
+            <motion.h2 variants={titleAnimation}>come true</motion.h2>
           </div>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videograpghy ideas that you have. We
           have professionals with amazing skills.
