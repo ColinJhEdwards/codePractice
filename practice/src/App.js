724124a23be24d16d341eb8ba9551f7349e14400
Colin Edwards <ColinJhEdwards@gmail.com>
2022-10-25 08:21:26 -0700

    used useState for storing and displaying song data

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import Song from "./components/Song";
 import Player from "./components/Player";
 import "./styles/app.scss";
+import data from "./data";
 
 function App() {
+  // state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
