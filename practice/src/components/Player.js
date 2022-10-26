98a89e094d899322095d4a5d231bf859592b68ff
Colin Edwards <ColinJhEdwards@gmail.com>
2022-10-26 08:22:57 -0700

    added functionality to display time for song

--- practice/src/components/Player.js
+++ practice/src/components/Player.js
@@ -6,16 +6,38 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong }) => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const audioRef = useRef(null);
   // event handlers
-  const playSongHandler = () => {};
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
+        <p>{getTime(songInfo.currentTime) || "0:00"}</p>
         <input type="range" />
-        <p>End Time</p>
+        <p>{getTime(songInfo.duration) || "0:00"}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} />
@@ -26,7 +48,11 @@ const Player = ({ currentSong }) => {
         />
         <FontAwesomeIcon className="skip-forward" icon={faAngleRight} />
       </div>
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+        ref={audioRef}
+        src={currentSong.audio}
+        onTimeUpdate={timeUpdateHandler}
+      ></audio>
     </div>
   );
 };
